30a8b3b95045aed819b550f53be5689e1e5eb2d7
JoflynRodrigues <JOFLYNRODS31@GMAIL.COM>
2026-04-09 15:53:31 +0200

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2026 12:06:32
-// Design Name: 
-// Module Name: control_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module control_unit(
     input[31:0]instr,
     output reg Regwrite,Memread,Memwrite,ALUsrc,MemToReg,
