8b6e9cfd19048f3bc9ca4704bac59a03d9fca3aa
JoflynRodrigues <JOFLYNRODS31@GMAIL.COM>
2026-04-09 15:51:57 +0200

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2026 11:50:25
-// Design Name: 
-// Module Name: regfile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module regfile(
     input clk,
     input [4:0]rs1,rs2,rd,
