648851a1d9d2723d3719390272fef9caeebb3f78
JoflynRodrigues <JOFLYNRODS31@GMAIL.COM>
2026-04-09 15:52:16 +0200

    Update riscv_singlecycle.v

--- riscv_singlecycle.v
+++ riscv_singlecycle.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2026 12:50:39
-// Design Name: 
-// Module Name: riscv_singlecycle
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module riscv_singlecycle(
     input clk,
     input reset
