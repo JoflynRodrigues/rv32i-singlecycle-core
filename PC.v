ba11a2b46e6d8593df35dbbd9d98dc558baf0f63
JoflynRodrigues <JOFLYNRODS31@GMAIL.COM>
2026-04-09 15:50:54 +0200

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.03.2026 16:03:03
-// Design Name: 
-// Module Name: PC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module pc(
 
     input clk,
